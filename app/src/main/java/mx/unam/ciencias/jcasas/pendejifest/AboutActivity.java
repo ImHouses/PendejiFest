dd467d05ac1f1891e9b16fb8adda2aa468f87532
jcasas <imhouses@ciencias.unam.mx>
2016-10-08 12:33:37 -0500

    Added String values to String.xml, an arrow icon for the AppBar, customized the AppBar for elevation.

--- app/src/main/java/mx/unam/ciencias/jcasas/pendejifest/AboutActivity.java
+++ app/src/main/java/mx/unam/ciencias/jcasas/pendejifest/AboutActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.style.TextAppearanceSpan;
+import android.view.View;
 import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
@@ -18,6 +19,10 @@ public class AboutActivity extends AppCompatActivity {
         // Set a Toolbar to replace the ActionBar.
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_action_arrow_left);
+        toolbar.setNavigationOnClickListener((v) -> {
+            onBackPressed();
+        });
         tvA = (TextView)findViewById(R.id.textViewAbout);
         String aboutText = "Pendeji Fest App In Developing\n Release:" +
                             getResources().getString(R.string.date_dev).toString() +
